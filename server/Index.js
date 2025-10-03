05a91ffd7cead54a323d955c0cc8bb68a31e92d4
meghanad755-cloud <meghanad755@gmail.com>
2025-10-03 15:24:55 +0530

    final

--- server/Index.js
+++ server/Index.js
@@ -8,7 +8,12 @@ const app = express()
 const PORT = process.env.PORT;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 
 RunServer()
